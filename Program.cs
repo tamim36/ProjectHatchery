d4f098e07e86826f6990bce3d0870a4d3629d4fa
tamim36 <tamim36@student.sust.edu>
2020-01-14 13:24:56 +0600

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProjectHatchery
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
